87b35e6e48e2faad3ee3091f38f988ea44d08ed2
novr (Nobuhisa Komiya) <nobuhisa.komiya@accenture.com>
2026-04-06 14:07:22 +0900

    chore(example): use KawarimiConfigView(client:specType:) in demo

--- Example/DemoApp/DemoAppUI/HengeRootView.swift
+++ Example/DemoApp/DemoAppUI/HengeRootView.swift
@@ -5,25 +5,11 @@ import SwiftUI
 
 struct HengeRootView: View {
     /// Must match the OpenAPI client base URL or requests will not reach `__kawarimi`.
-    private var baseURL: URL {
-        KawarimiExampleConfig.clientBaseURL!
-    }
-
     private var client: KawarimiAPIClient {
-        KawarimiAPIClient(baseURL: baseURL)
+        KawarimiAPIClient(baseURL: KawarimiExampleConfig.clientBaseURL!)
     }
 
     var body: some View {
-        KawarimiConfigView(
-            serverURL: KawarimiExampleConfig.serverBaseURL,
-            specProvider: {
-                let spec: SpecResponse = try await client.fetchSpec(as: SpecResponse.self)
-                return (meta: spec.meta, endpoints: spec.endpoints)
-            },
-            fetchOverrides: { try await client.fetchOverrides() },
-            configureOverride: { try await client.configure(override: $0) },
-            removeOverride: { try await client.removeOverride(override: $0) },
-            resetAllOverrides: { try await client.reset() }
-        )
+        KawarimiConfigView(client: client, specType: SpecResponse.self)
     }
 }
